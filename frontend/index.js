87129f0bd6906d622ab8fb767caa75a03da60352
Ivan Valdes Riesco <ivaldes@everlyhealth.com>
2023-10-21 22:08:48 -0300

    fixes

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,19 @@
 const recordButton = document.getElementById('record-button');
 let mediaRecorder;
 let chunks = [];
+A = [
+  'armonía', 'agrupación', 'asociación', 'abeja', 'arveja',
+  'anteojos', 'antiparras', 'ancla', 'alcaparra', 'aceptar',
+  'aceptación', 'ancestro', 'Antropología', 'antropólogo',
+  'avion', 'arco', 'arquero', 'arremetida', 'agregar', 'ajedrez',
+  'ajenjo', 'acelga', 'apio', 'aceituna', 'agua', 'albahaca',
+  'alarma', 'asno', 'arepa', 'asma', 'alcachofa', 'avena', 'alegría',
+  'amarillo', 'azul', 'ayer', 'asiento', 'animal', 'artimaña', 'abrir'
+].map(word => word.replace(/s$]/g,""));
+let respose;
 
 recordButton.addEventListener('click', () => {
+  document.getElementById('result').innerText = '';
   if (mediaRecorder && mediaRecorder.state === 'recording') {
     mediaRecorder.stop();
     recordButton.innerText = 'Sending...';
@@ -36,7 +47,15 @@ recordButton.addEventListener('click', () => {
         })
         .then(response => response.json())
         .then(data => {
-          console.log('Success:', data);
+          response = data.transcription.split(' ')
+            .map(word => word.toLowerCase())
+            .map(word => word.replace(/s$]/g,""));
+          uniques = [...new Set(response)]
+          correct = uniques.filter(word => A.includes(word));
+          console.log(`API Response: ${response}`);
+          console.log(`Correct words: ${correct}`);
+          // fill the result id with a % of correct words
+          document.getElementById('result').innerText = `${correct.length / A.length * 100}%`;
           recordButton.innerText = 'Record';
         })
         .catch(error => {
