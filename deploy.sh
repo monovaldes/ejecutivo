3fc28374612593cacb0ec2646d5270f5c20f6e35
Ivan Valdes Riesco <ivaldes@everlyhealth.com>
2023-11-28 11:03:23 -0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 # gcloud auth login
-gcloud builds submit --tag gcr.io/prime-freedom-402713/ejecutivo .
 gsutil cp 404-page.html index.html index.js index.css gs://ejecutivoweb
+cd backend
+gcloud builds submit --tag gcr.io/prime-freedom-402713/ejecutivo .
+cd ..
